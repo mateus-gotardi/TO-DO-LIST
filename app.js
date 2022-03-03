1c1c31d63c733a0fb1c1db236647f60850cb970d
mateus gotardi <mateus.gotardi@gmail.com>
2022-03-03 17:42:05 -0300

    nodemon

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express')
 
+const app = express()
+
+app.listen(3000,()=>{
+    console.log("servidor iniciado")
+})
+
+app.get ('/', (req, res)=>{
+    res.end('<h1>Minha lista de tarefas</h1>')
+})
+app.get('/json', (req,res)=>{
+    res.json({
+        title: 'tarefa X',  done: true
+    })
+})
