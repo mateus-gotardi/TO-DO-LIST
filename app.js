ff8663923b287a3af68397c026de22aa89c83184
mateus gotardi <mateus.gotardi@gmail.com>
2022-03-03 18:36:59 -0300

    ...

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ app.listen(3000,()=>{
     console.log("servidor iniciado")
 })
 
+app.use(express.json())
+
+const log = (req, res, next)=>{
+    console.log(req.body)
+    console.log(`Data:`)
+    next()
+}
+
+app.use(log)
+
 app.get ('/', (req, res)=>{
     res.end('<h1>Minha lista de tarefas</h1>')
 })
+
 app.get('/json', (req,res)=>{
     res.json({
         title: 'tarefa X',  done: true
